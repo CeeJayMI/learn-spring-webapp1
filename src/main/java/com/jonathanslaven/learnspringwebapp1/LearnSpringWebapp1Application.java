5a9e32a203c39eaa164c5b93e0edc965afe81053
ceejaymi <jonathan@jonathanslaven.com>
2022-06-06 13:58:15 -0400

    Initial commit

---
+++ src/main/java/com/jonathanslaven/learnspringwebapp1/LearnSpringWebapp1Application.java
@@ -0,0 +1,13 @@
+package com.jonathanslaven.learnspringwebapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringWebapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringWebapp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonathanslaven/learnspringwebapp1/LearnSpringWebapp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathanslaven.learnspringwebapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringWebapp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
